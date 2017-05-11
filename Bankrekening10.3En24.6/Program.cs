f0685918ad4312c4e31a1eb9277f875def2466c4
timour.meeusen <timour.meeusen@B209-11>
2017-05-11 11:13:18 +0200

    Add project files.

---
+++ Bankrekening10.3En24.6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankrekening10._3En24._6
+{
+    class Program
+    {
+        private static void ExecuteerMenu()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
