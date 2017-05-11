c0c661e9c2c4a61c97c935adcaf457b22c39933c
timour.meeusen <timour.meeusen@B209-11>
2017-05-11 11:18:16 +0200

    Bankrekening

---
+++ Bankrekening10.3En24.6/Bankrekening.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankrekening10._3En24._6
+{
+    class Bankrekening
+    {
+        public void Storten()
+        {
+
+        }
+
+        public void Opnemen()
+        {
+
+        }
+
+        public double HuidigSaldo { get; set; }
+    }
+}
